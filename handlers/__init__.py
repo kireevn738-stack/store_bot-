34c2d293586048b0f492f5484222ddd392ce1dc6
kireevn738-stack <kireevn738@gmail.com>
2026-01-03 02:26:47 +0500

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,11 @@
+from .start import router as start_router
+from .products import router as products_router
+from .categories import router as categories_router
+from .analytics import router as analytics_router
+
+all = [
+    'start_router',
+    'products_router',
+    'categories_router',
+    'analytics_router'
+]
