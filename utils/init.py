a6dc521e9b6eacb14beaa4bbbdbaeda1d396aa7e
kireevn738-stack <kireevn738@gmail.com>
2026-01-03 21:33:58 +0500

    Create init.py

---
+++ utils/init.py
@@ -0,0 +1,13 @@
+from .validators import (
+    is_valid_email,
+    is_valid_price,
+    is_valid_quantity,
+    parse_date
+)
+
+__all__ = [
+    'is_valid_email',
+    'is_valid_price',
+    'is_valid_quantity',
+    'parse_date'
+]
