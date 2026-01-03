354b32c969b3cda33b1ff6e8fa2b63dc3fa6f1c6
kireevn738-stack <kireevn738@gmail.com>
2026-01-03 21:25:22 +0500

    Create init.py

---
+++ keyboards/init.py
@@ -0,0 +1,19 @@
+from .builders import (
+    get_language_keyboard,
+    get_main_menu_keyboard,
+    get_cancel_keyboard,
+    get_analytics_period_keyboard,
+    get_product_actions_keyboard,
+    get_categories_keyboard,
+    get_yes_no_keyboard
+)
+
+__all__ = [
+    'get_language_keyboard',
+    'get_main_menu_keyboard',
+    'get_cancel_keyboard',
+    'get_analytics_period_keyboard',
+    'get_product_actions_keyboard',
+    'get_categories_keyboard',
+    'get_yes_no_keyboard'
+]
