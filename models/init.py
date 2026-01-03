0dfddf0e1e7d19181252911c40b4b2734f1d8a1a
kireevn738-stack <kireevn738@gmail.com>
2026-01-03 21:30:38 +0500

    Create init.py

---
+++ models/init.py
@@ -0,0 +1,23 @@
+from .models import (
+    UserCreate,
+    UserUpdate,
+    CategoryCreate,
+    CategoryUpdate,
+    ProductCreate,
+    ProductUpdate,
+    OrderItemCreate,
+    OrderCreate,
+    AnalyticsRequest
+)
+
+__all__ = [
+    'UserCreate',
+    'UserUpdate',
+    'CategoryCreate',
+    'CategoryUpdate',
+    'ProductCreate',
+    'ProductUpdate',
+    'OrderItemCreate',
+    'OrderCreate',
+    'AnalyticsRequest'
+]
