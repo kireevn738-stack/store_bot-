7f79cc19c4a70319b5b842ef3ae184ffbe67debb
kireevn738-stack <kireevn738@gmail.com>
2026-01-03 22:09:00 +0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -88,7 +88,7 @@ class OrderCreate(BaseModel):
         return v
 
 class AnalyticsRequest(BaseModel):
-    period: str 
+    period: str
     start_date: Optional[datetime] = None
     end_date: Optional[datetime] = None
     
