02e4021ba04a9e3a430e06a8dcdcd8e2fc983c9b
kireevn738-stack <kireevn738@gmail.com>
2026-01-03 21:11:31 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,19 @@
 from .start import router as start_router
+from .registration import router as registration_router
+from .language import router as language_router
+from .store import router as store_router
 from .products import router as products_router
 from .categories import router as categories_router
+from .orders import router as orders_router
 from .analytics import router as analytics_router
 
-all = [
-    'start_router',
-    'products_router',
-    'categories_router',
-    'analytics_router'
+routers = [
+    start_router,
+    registration_router,
+    language_router,
+    store_router,
+    products_router,
+    categories_router,
+    orders_router,
+    analytics_router
 ]
