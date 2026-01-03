d349c7ae08beac4f6a2e48492009a99277db0cce
kireevn738-stack <kireevn738@gmail.com>
2026-01-03 22:54:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(','))) if os.getenv("ADMIN_IDS") else []
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///store_bot.db")
@@ -14,5 +14,3 @@ LANGUAGES = {
 }
 
 DEFAULT_LANGUAGE = 'ru'
-
-PORT = int(os.getenv("PORT", 8080))
